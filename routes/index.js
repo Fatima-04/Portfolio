8edd6e7c6e1d7ceda8c04b8cd78dc40d0c6a5252
Fatima-04 <146798356+Fatima-04@users.noreply.github.com>
2023-10-03 23:00:13 -0400

    refactored code

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const a = [
-    { title: "ABC", body: "dft fg ftftty yt" },
-    { title: "DEF", body: "dfhghft fg ftfghgfhgfhtty yt" },
-    { title: "ghghg", body: "hjgkjhg hj hj jhj jh" },
-  ];
-  res.render("index", { title: "Express", test: a });
+  res.render("index", { title: "Home Page", page: "home" });
 });
 
-router.get("/abc", function (req, res, next) {
-  const a = [
-    { title: "ABC", body: "dft fg ftftty yt" },
-    { title: "DEF", body: "dfhghft fg ftfghgfhgfhtty yt" },
-    { title: "ghghg", body: "hjgkjhg hj hj jhj jh" },
-  ];
-  res.render("index", { title: "about", test: a });
+router.get("/home", function (req, res, next) {
+  res.render("index", { title: "Home Page", page: "home" });
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("index", { title: "About Me", page: "about" });
+});
+
+router.get("/services", function (req, res, next) {
+  res.render("index", { title: "Services", page: "services" });
+});
+
+router.get("/projects", function (req, res, next) {
+  res.render("index", { title: "Projects", page: "projects" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("index", { title: "Contact Me", page: "contact" });
+});
+
+router.post("/PageSubmit", function (req, res, next) {
+  res.render("index", { title: "home page", page: "home" });
 });
 
 module.exports = router;
